b512ba1aa173e524793d0445e1bb49a51368ca53
Logan Thomas <Thomas.lo@husky.neu.edu>
2015-06-30 13:42:28 -0400

    ADCommando

---
+++ ADCommando.py
@@ -0,0 +1,166 @@
+import sys
+import time
+import serial
+import ctypes
+
+try:
+    import pycomando
+except ImportError:
+    sys.path.append('../../')
+    import pycomando
+
+def show(bs):
+    print("[echo]->%r" % bs)
+
+class ADCommando(object):
+    def __init__(self, port='COM5',rate=9600,board_ind=0):
+        self.board_ind = board_ind
+        self.con = serial.Serial(port,rate)
+        time.sleep(1)
+        self.con.setDTR(level=0)
+        time.sleep(1)
+        self.com = pycomando.Comando(self.con)
+        self.text = pycomando.protocols.TextProtocol(self.com)
+        self.cmd = pycomando.protocols.CommandProtocol(self.com)  
+        self.com.register_protocol(0, self.text)
+        self.com.register_protocol(1, self.cmd)
+        self.text.receive_message = self.show
+        
+    def show(self, bs):
+        print("[echo]->%r" % bs)
+
+    def configure(self):
+        """
+        reconfigures board with updated settings
+        """
+        self.cmd.send_command(0,(self.board_ind,))
+        time.sleep(.5)
+        while self.con.inWaiting():
+            self.com.handle_stream()
+        
+    def soft_stop(self):
+        """
+        brings the motor to a soft stop
+        """
+        self.cmd.send_command(1,(self.board_ind,))
+        time.sleep(.5)
+        while self.con.inWaiting():
+            self.com.handle_stream()
+        
+    def hard_stop(self):
+        """
+        brings the board to a hard stop
+        """
+        self.cmd.send_command(2,(self.board_ind,))
+        time.sleep(.5)
+        while self.con.inWaiting():
+            self.com.handle_stream()
+    
+    def release(self):
+        """
+        releases the board
+        """
+        self.cmd.send_command(3,(self.board_ind,))
+        time.sleep(.5)
+        while self.con.inWaiting():
+            self.com.handle_stream()
+        
+    def set_max_speed(self, max_sp):
+        """
+        sets the max speed to integer value specified
+        """
+        if type(max_sp) != int:
+            max_sp = int(max_sp)
+        self.cmd.send_command(4,(self.board_ind,max_sp))
+        time.sleep(.5)
+        while self.con.inWaiting():
+            self.com.handle_stream()
+     
+    def set_accel(self, accel):
+        """
+        sets accel and decell to value specified
+        """
+        if type(accel) != int:
+            accel = int(accel)
+        self.cmd.send_command(5,(self.board_ind,accel))
+        time.sleep(.5)
+        while self.con.inWaiting():
+            self.com.handle_stream()
+        
+    def set_current(self, k_val):
+        """
+        sets the k value (must be int between 0 and 255)
+        """
+        if type(k_val) != int:
+            k_val = int(k_val)
+        self.cmd.send_command(6,(self.board_ind,k_val))
+        time.sleep(.5)
+        while self.con.inWaiting():
+            self.com.handle_stream()
+        
+    def set_microstepping(self, ms):
+        """
+        sets the microstepping value int power
+        i.e 7 = 2^7 
+        """
+        if type(ms) != int:
+            ms = int(ms)
+        self.cmd.send_command(7,(self.board_ind,ms))
+        time.sleep(.5)
+        while self.con.inWaiting():
+            self.com.handle_stream()
+
+    def low_speed_mode(self, enabled):
+        """
+        sets low speed mode to on or off
+        """
+        if type(enabled) != bool:
+            enabled = bool(enabled)
+        self.cmd.send_command(8,(self.board_ind,enabled))
+        time.sleep(.5)
+        while self.con.inWaiting():
+            self.com.handle_stream()
+        
+    def is_moving(self):
+        """
+        checks if board is moving
+        """
+        self.cmd.send_command(9,(self.board_ind,))
+        time.sleep(.5)
+        while self.con.inWaiting():
+            self.com.handle_stream()
+        
+    def wait(self):
+        """
+        waits until the arduino is done with its current operation
+        """
+        self.cmd.send_command(10,(self.board_ind,))
+        time.sleep(.5)
+        while self.con.inWaiting():
+            self.com.handle_stream()
+        
+    def rotate(self,dir, sps):
+        """
+        rotates the motor in the direction of dir at sps steps per second
+        """
+        if type(dir) != int:
+            dir = int(dir)
+        if type(sps) != int:
+            sps = int(sps)
+        self.cmd.send_command(11,(self.board_ind,dir,sps))
+        time.sleep(.5)
+        while self.con.inWaiting():
+            print(self.con.readline())
+        
+    def move_steps(self,dir, steps):
+        """
+        moves number of steps in the direction specified
+        """
+        if type(dir) != int:
+            dir = int(dir)
+        if type(steps) != int:
+            steps = int(steps)
+        self.cmd.send_command(12,(self.board_ind,dir,steps))
+        time.sleep(.1)
+        while self.con.inWaiting():
+            self.com.handle_stream()
---
+++ ADCommando/ADCommando.ino
@@ -0,0 +1,294 @@
+#include <AutoDriver.h>
+#include<comando.h>
+//#define DEBUG 1
+Comando com = Comando(Serial);
+EchoProtocol echo = EchoProtocol(com);
+CommandProtocol cmd =  CommandProtocol(com);
+
+class AutoReel: public AutoDriver {
+  private:
+    int max_speed;
+    int acc;
+    int dec;
+    int k;
+    int ms;
+    boolean low_speed;
+    int _bp;
+    
+  public:
+    AutoReel(int CSPin, int resetPin, int busyPin)
+    : AutoDriver(CSPin, resetPin, busyPin),
+      max_speed(500),
+      acc(500),
+      dec(500),
+      k(41),
+      ms(7),
+      low_speed(false),
+      _bp(busyPin)
+      {}
+    void configure () {
+ 
+      //resetDev();
+      configSyncPin(_bp, 0);
+      configStepMode(ms);
+      setMaxSpeed(max_speed);
+      //setFullSpeed(600);
+      setLoSpdOpt(low_speed);
+      setAcc(acc);
+      setDec(dec);
+      setSlewRate(SR_530V_us);
+      setOCThreshold(OC_750mA);//OC_6000mA
+      setPWMFreq(PWM_DIV_2, PWM_MUL_2);
+      setOCShutdown(OC_SD_DISABLE);
+      setVoltageComp(VS_COMP_DISABLE);
+      setSwitchMode(SW_USER);
+      setOscMode(INT_16MHZ_OSCOUT_16MHZ);
+      setAccKVAL(k);
+      setDecKVAL(k);
+      setRunKVAL(k);
+      setHoldKVAL(k);
+      #ifdef DEBUG
+        Serial.println(getDecKVAL());
+        Serial.println(getRunKVAL());
+        Serial.println(getHoldKVAL());
+        Serial.println(getMaxSpeed());
+        Serial.println(getFullSpeed());
+        Serial.println(getAcc());
+        Serial.println(getMinSpeed());
+        Serial.println(getStepMode());
+        Serial.println(getLoSpdOpt());
+        Serial.println(getOCThreshold());
+        Serial.println(getPWMFreqDivisor());
+        Serial.println(getPWMFreqMultiplier());
+        Serial.println(getVoltageComp());
+        Serial.println(getSwitchMode());
+      #endif
+    }
+    
+    void set_max_speed(int v) {
+      #ifdef DEBUG
+        Serial.print("~set max_speed to ");
+        Serial.println(v, DEC);
+      #endif
+      max_speed = v;
+      //configure();
+    }
+    
+    int get_max_speed() {
+      return max_speed;
+    }
+    
+    void set_acc(int v) {
+      #ifdef DEBUG
+        Serial.print("~set acc to ");
+        Serial.println(v, DEC);
+      #endif
+      acc = v;
+      //configure();
+    }
+    
+    int get_acc() {
+      return acc;
+    }
+    
+    void set_dec(int v) {
+      #ifdef DEBUG
+        Serial.print("~set dec to ");
+        Serial.println(v, DEC);
+      #endif
+      dec = v;
+      //configure();
+    }
+    
+    int get_dec() {
+      return dec;
+    }
+    
+    void set_k(int v) {
+      #ifdef DEBUG
+        Serial.print("~set k to ");
+        Serial.println(v, DEC);
+      #endif
+      k = v;
+      //configure();
+    }
+    
+    int get_k() {
+      return k;
+    }
+    
+    void set_ms(int v) {
+      #ifdef DEBUG
+        Serial.print("~set ms to ");
+        Serial.println(v, DEC);
+      #endif
+      ms = v;
+      //configure();
+    };
+    
+    int get_ms() {
+      return ms;
+    };
+    
+    void set_low_speed(boolean v) {
+      #ifdef DEBUG
+        Serial.print("~set low_speed to ");
+        Serial.println(v, DEC);
+      #endif
+      low_speed = v;
+      //configure();
+    };
+    
+    boolean get_low_speed() {
+      return low_speed;
+    };
+};
+
+AutoReel boards[] = {
+  AutoReel(10,6,5),
+};
+
+byte nboards = 0;
+byte board_index = 0;
+
+void conf(CommandProtocol* cmd){ //configure
+  echo.send_message(com.get_bytes(), com.get_n_bytes());
+  int board = cmd->get_arg<int>();
+  boards[board].configure();
+};//0
+
+void soft_stop(CommandProtocol* cmd){ //stop
+  echo.send_message(com.get_bytes(), com.get_n_bytes());
+  int board = cmd->get_arg<int>();
+  boards[board].softStop();
+};//1
+
+void hard_stop(CommandProtocol* cmd){ //hard stop
+  echo.send_message(com.get_bytes(), com.get_n_bytes());
+  int board = cmd->get_arg<int>();
+  boards[board].hardStop();
+};//2
+
+void rel(CommandProtocol* cmd){ //release
+  echo.send_message(com.get_bytes(), com.get_n_bytes());
+  int board = cmd->get_arg<int>();
+  boards[board].softHiZ();
+};//3
+
+void max_sp(CommandProtocol* cmd){ //set max speed
+  echo.send_message(com.get_bytes(), com.get_n_bytes());
+  int board = cmd->get_arg<int>();
+  int ex = cmd->get_arg<int>();
+  int sp = cmd->get_arg<int>();
+  boards[board].set_max_speed(sp);
+};//4
+
+void set_accel(CommandProtocol* cmd){ //set acc/dec
+  echo.send_message(com.get_bytes(), com.get_n_bytes());
+  int board = cmd->get_arg<int>();
+  int ex = cmd->get_arg<int>();
+  int ac = cmd->get_arg<int>();
+  boards[board].set_acc(ac);
+  boards[board].set_dec(ac);
+};//5
+
+void set_current(CommandProtocol* cmd){ //set k value [0-255]
+  echo.send_message(com.get_bytes(), com.get_n_bytes());
+  int board = cmd->get_arg<int>();
+  int ex = cmd->get_arg<int>();
+  int new_k = cmd->get_arg<int>();
+  boards[board].set_k(new_k);
+};//6
+
+void set_micro(CommandProtocol* cmd){ //set microstepping vals [0-7]
+  echo.send_message(com.get_bytes(), com.get_n_bytes());
+  int board = cmd->get_arg<int>();
+  int ex = cmd->get_arg<int>();
+  int new_ms = cmd->get_arg<int>();
+  boards[board].set_ms(new_ms);
+};//7
+
+void low_speed(CommandProtocol* cmd){ //set low speed mode true or false
+  echo.send_message(com.get_bytes(), com.get_n_bytes());
+  int board = cmd->get_arg<int>();
+  int ex = cmd->get_arg<int>();
+  bool is_on = cmd->get_arg<bool>();
+  boards[board].set_low_speed(is_on);
+  boards[board].configure();
+};//8
+
+void is_moving(CommandProtocol* cmd){ //returns a bool if the board is moving
+  echo.send_message(com.get_bytes(), com.get_n_bytes());
+  int board = cmd->get_arg<int>();
+  bool is_moving = boards[board].busyCheck();
+  //echo.send_message(is_moving);
+};//9
+
+void wait(CommandProtocol* cmd){ //holds until the board is done moving
+  echo.send_message(com.get_bytes(), com.get_n_bytes());
+  int board = cmd->get_arg<int>();
+  while(!boards[board].busyCheck());
+};//10
+
+void rot(CommandProtocol* cmd){ //rotates int dir int steps_per_sec
+  //echo.send_message(com.get_bytes(), com.get_n_bytes());
+  int board = cmd->get_arg<int>();
+  int ex = cmd->get_arg<int>();
+  int dir = cmd->get_arg<int>();
+  int ex2 = cmd->get_arg<int>();
+  int sps = cmd->get_arg<int>();
+  
+  
+  Serial.println(board);
+  Serial.println(ex);
+  Serial.println(dir);
+  Serial.println(ex2);
+  Serial.println(sps);
+  Serial.println(boards[board].get_ms());
+  boards[board].run(dir,sps);
+};//11
+
+void move_steps(CommandProtocol* cmd){ //move int dir int num_steps
+  echo.send_message(com.get_bytes(), com.get_n_bytes());
+  int board = cmd->get_arg<int>();
+  int ex = cmd->get_arg<int>();
+  int dir = cmd->get_arg<int>();
+  ex = cmd->get_arg<int>();
+  int num_steps = cmd->get_arg<int>();
+  boards[board].move(dir,num_steps);
+};//12
+
+void query(CommandProtocol* cmd){//returns all vital values
+  echo.send_message(com.get_bytes(),com.get_n_bytes());
+};
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  nboards = sizeof(boards) / sizeof(AutoReel);
+  for (int i=0; i < nboards; i++) {
+    boards[i].configure();
+  };
+  // register protocols
+  com.register_protocol(0,echo);
+  com.register_protocol(1,cmd);
+  // register command callbacks
+  cmd.register_callback(0,conf);
+  cmd.register_callback(1,soft_stop);
+  cmd.register_callback(2,hard_stop);
+  cmd.register_callback(3,rel);
+  cmd.register_callback(4,max_sp);
+  cmd.register_callback(5,set_accel);
+  cmd.register_callback(6,set_current);
+  cmd.register_callback(7,set_micro);
+  cmd.register_callback(8,low_speed);
+  cmd.register_callback(9,is_moving);
+  cmd.register_callback(10,wait);
+  cmd.register_callback(11,rot);
+  cmd.register_callback(12,move_steps);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  com.handle_stream();
+}
