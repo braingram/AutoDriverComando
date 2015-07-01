c43006b6b24d613bab134fdd5246490a7e1206be
Logan Thomas <Thomas.lo@husky.neu.edu>
2015-07-01 16:55:04 -0400

    changing types to ctypes int16

--- adcomando/__init__.py
+++ adcomando/__init__.py
@@ -7,10 +7,23 @@ import pycomando
 def show(bs):
     print("[echo]->%r" % bs)
 
+        
+def wait_response(func):
+    """
+    Decorator that waits for arduino to respond after the function is called
+    """
+    def inner(self, *args, **kwargs):
+        func(self, *args, **kwargs)
+        while self.con.inWaiting():
+            self.com.handle_stream()
+    inner.__name__ = func.__name__
+    inner.__doc__ = func.__doc__
+    return inner
 
+    
 class AutoDriverComando(object):
     def __init__(self, port='COM5', rate=9600, board_ind=0):
-        self.board_ind = board_ind
+        self.board_ind = ctypes.c_int16(board_ind)
         self.con = serial.Serial(port, rate)
         time.sleep(1)
         self.con.setDTR(level=0)
@@ -35,92 +48,60 @@ class AutoDriverComando(object):
         reconfigures board with updated settings
         """
         self.cmd.send_command(0, (self.board_ind, ))
-        time.sleep(.5)
-        while self.con.inWaiting():
-            self.com.handle_stream()
 
     def soft_stop(self):
         """
         brings the motor to a soft stop
         """
         self.cmd.send_command(1, (self.board_ind, ))
-        time.sleep(.5)
-        while self.con.inWaiting():
-            self.com.handle_stream()
 
     def hard_stop(self):
         """
         brings the board to a hard stop
         """
         self.cmd.send_command(2, (self.board_ind, ))
-        time.sleep(.5)
-        while self.con.inWaiting():
-            self.com.handle_stream()
 
     def release(self):
         """
         releases the board
         """
         self.cmd.send_command(3, (self.board_ind, ))
-        time.sleep(.5)
-        while self.con.inWaiting():
-            self.com.handle_stream()
 
     def set_max_speed(self, max_sp):
         """
         sets the max speed to integer value specified
         """
-        if type(max_sp) != int:
-            max_sp = int(max_sp)
+        max_sp = ctypes.c_int16(max_sp)
         self.cmd.send_command(4, (self.board_ind, max_sp))
-        time.sleep(.5)
-        while self.con.inWaiting():
-            self.com.handle_stream()
 
     def set_accel(self, accel):
         """
         sets accel and decell to value specified
         """
-        if type(accel) != int:
-            accel = int(accel)
+        accel = ctypes.c_int16(accel)
         self.cmd.send_command(5, (self.board_ind, accel))
-        time.sleep(.5)
-        while self.con.inWaiting():
-            self.com.handle_stream()
 
     def set_current(self, k_val):
         """
         sets the k value (must be int between 0 and 255)
         """
-        if type(k_val) != int:
-            k_val = int(k_val)
+        k_val = ctypes.c_int16(k_val)
         self.cmd.send_command(6, (self.board_ind, k_val))
-        time.sleep(.5)
-        while self.con.inWaiting():
-            self.com.handle_stream()
 
     def set_microstepping(self, ms):
         """
         sets the microstepping value int power
         i.e 7 = 2^7
         """
-        if type(ms) != int:
-            ms = int(ms)
+        ms = ctypes.c_int16(ms)
         self.cmd.send_command(7, (self.board_ind, ms))
-        time.sleep(.5)
-        while self.con.inWaiting():
-            self.com.handle_stream()
 
     def low_speed_mode(self, enabled):
         """
         sets low speed mode to on or off
         """
-        if type(enabled) != bool:
-            enabled = bool(enabled)
+        enabled = bool(enabled)
         self.cmd.send_command(8, (self.board_ind, enabled))
-        time.sleep(.5)
-        while self.con.inWaiting():
-            self.com.handle_stream()
 
     def is_moving(self):
         """
@@ -154,24 +135,14 @@ class AutoDriverComando(object):
         """
         rotates the motor in the direction of dir at sps steps per second
         """
-        if type(direction) != int:
-            direction = int(direction)
-        if type(sps) != int:
-            sps = int(sps)
+        direction = ctypes.c_int16(direction)
+        sps = ctypes.c_int16(sps)
         self.cmd.send_command(11, (self.board_ind, direction, sps))
-        time.sleep(.5)
-        while self.con.inWaiting():
-            self.com.handle_stream()
 
     def move_steps(self, direction, steps):
         """
         moves number of steps in the direction specified
         """
-        if type(direction) != int:
-            direction = int(direction)
-        if type(steps) != int:
-            steps = int(steps)
+        direction = ctypes.c_int16(direction)
+        steps = ctypes.c_int16(steps)
         self.cmd.send_command(12, (self.board_ind, direction, steps))
-        time.sleep(.1)
-        while self.con.inWaiting():
-            self.com.handle_stream()
